aeb8e4c21f834fb7340ffa42ac4157193c8032ec
Frederic Jacobs <github@fredericjacobs.com>
2016-11-18 12:34:06 +0100

    New share format. (#13)
    * Removing Coverall support until fixed. (See #12)
    
    * Changing Cargo license.
    
    * New wrapping of share data to support signatures.

--- tests/test_vectors.rs
+++ tests/test_vectors.rs
@@ -1,13 +1,35 @@
 extern crate rusty_secrets;
+extern crate protobuf;
+extern crate rustc_serialize as serialize;
+use serialize::base64::{ self, FromBase64, ToBase64 };
 
-use rusty_secrets::{recover_secret};
+use protobuf::Message;
+#[cfg(test)]
+use rusty_secrets::{recover_secret, ShareData};
+
+pub fn wrap_from_sellibitze(share: &str) -> String {
+    let parts: Vec<_> = share.trim().split('-').collect();
+    let share_data = parts[2].from_base64().unwrap();
+
+    let config = base64::Config {
+        pad: false,
+        ..base64::STANDARD
+    };
+
+    let mut share_protobuf = ShareData::new();
+    share_protobuf.set_shamirData(share_data);
+
+    let b64_share = share_protobuf.write_to_bytes().unwrap().to_base64(config);
+
+    format!("{}-{}-{}", parts[0], parts[1], b64_share)
+}
 
 #[test]
 fn test_recover_sellibitze() {
-    let share1 = "2-1-1YAYwmOHqZ69jA".to_string();
-    let share2 = "2-4-F7rAjX3UOa53KA".to_string();
+    let share1 = "2-1-1YAYwmOHqZ69jA";
+    let share2 = "2-4-F7rAjX3UOa53KA";
 
-    let shares = vec![share1, share2];
+    let shares = vec![share1, share2].iter().map(|x| wrap_from_sellibitze(x)).collect::<Vec<_>>();
 
     let mut secret = "My secret".to_string().into_bytes();
     secret.push(10);
@@ -23,7 +45,7 @@ fn test_recover_es_test_vectors() {
     let share4 = "5-6-yyVPUeaYPPiWK0wIV5OQ/t61V0lSEO+7X++EWeHRlIq3sRBNwUpKNfx/C+Vc9xTzUftrqBKvkWDZQal7nyi2".to_string();
     let share5 = "5-7-i8iL6bVf272B3qIjp0QqSny6AIm+DkP7oQjkVVLvx9EMhlvd4HJOxPpmtNF/RjA/zz21d7DY/B//saOPpBQa".to_string();
 
-    let shares = vec![share1, share2, share3, share4, share5];
+    let shares = vec![share1, share2, share3, share4, share5].iter().map(|x| wrap_from_sellibitze(x)).collect::<Vec<_>>();
 
     let secret = "The immoral cannot be made moral through the use of secret law.".to_string().into_bytes();
     assert_eq!(recover_secret(shares).unwrap(), secret);
@@ -36,7 +58,7 @@ fn test_recover_sellibitze_more_than_threshold_shars() {
     let share3 = "2-2-YJZQDGm22Y77Gw".to_string();
     let share4 = "2-5-j0P4PHsw4lW+rg".to_string();
 
-    let shares = vec![share1, share2, share3, share4];
+    let shares = vec![share1, share2, share3, share4].iter().map(|x| wrap_from_sellibitze(x)).collect::<Vec<_>>();
 
     let mut secret = "My secret".to_string().into_bytes();
     secret.push(10);
