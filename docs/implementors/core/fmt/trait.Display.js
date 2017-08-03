b0e6aaf0e3a1bc0a9aa721e825f7418e525c76d9
Romain Ruetschi <romain.ruetschi@gmail.com>
2017-08-03 17:04:34 +0200

    Update documentation.

--- docs/implementors/core/fmt/trait.Display.js
+++ docs/implementors/core/fmt/trait.Display.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["rusty_secrets"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='rusty_secrets/struct.RustyError.html' title='rusty_secrets::RustyError'>RustyError</a>",];
+implementors["rusty_secrets"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"rusty_secrets/struct.RustyError.html\" title=\"struct rusty_secrets::RustyError\">RustyError</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
