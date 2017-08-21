ad73277295b926e9aca278d0c2570a435c688e41
Romain Ruetschi <romain.ruetschi@gmail.com>
2017-08-22 00:44:46 +0200

    Wording of error messages.

--- src/dss/errors.rs
+++ src/dss/errors.rs
@@ -10,13 +10,13 @@ error_chain! {
     errors {
         /// k must be smaller than or equal to n
         InvalidSplitParametersSmaller(k: u8, n: u8) {
-            description("The parameter k must be smaller than or equal to n")
-            display("The parameter k must be smaller than or equal to n, got: k = {}, n = {}", k, n)
+            description("Parameter k must be smaller than or equal to n")
+            display("Parameter k must be smaller than or equal to n, got: k = {}, n = {}.", k, n)
         }
 
         EmptyShares {
-            description("No shares were provided")
-            display("No shares were provided")
+            description("No shares provided")
+            display("No shares were provided.")
         }
 
         IncompatibleSets {
@@ -26,7 +26,7 @@ error_chain! {
 
         ShareIdentifierTooBig(id: u8, n: u8) {
             description("Share identifier too big")
-            display("Found share identifier ({}) bigger than the maximum number of shares ({})", id, n)
+            display("Found share identifier ({}) bigger than the maximum number of shares ({}).", id, n)
         }
 
         MissingShares(provided: usize, required: usize) {
@@ -35,23 +35,23 @@ error_chain! {
         }
 
         InvalidT2Parameters(r: usize, s: usize) {
-            description("invalid parameters for the T2 sharing scheme")
-            display("invalid parameters for the T2 sharing scheme: r = {}, s = {}", r, s)
+            description("Invalid parameters for the T2 sharing scheme")
+            display("Invalid parameters for the T2 sharing scheme: r = {}, s = {}.", r, s)
         }
 
         InvalidSplitParametersZero(k: u8, n: u8) {
-            description("k and n must be greater than zero")
-            display("k and n must be greater than zero")
+            description("Parameters k and n must be greater than zero")
+            display("Parameters k and n must be greater than zero.")
         }
 
         MismatchingShares(got: t2::Share, expected: t2::Share) {
             description("Share mismatch during verification of secret recovery")
-            display("Share mismatch during verification of secret recovery")
+            display("Share mismatch during verification of secret recovery.")
         }
 
         CannotGenerateRandomNumbers {
             description("Cannot generate random numbers")
-            display("Cannot generate random numbers")
+            display("Cannot generate random numbers.")
         }
 
         DuplicateShareId(share_id: u8) {
@@ -60,8 +60,8 @@ error_chain! {
         }
 
         DuplicateShareData(share_id: u8) {
-            description("the data encoded in this share is the same as the one found in a previous share")
-            display("the data encoded in share #{} is the same as the one found in a previous share", share_id)
+            description("The data encoded in this share is the same as the one found in a previous share")
+            display("The data encoded in share #{} is the same as the one found in a previous share.", share_id)
         }
 
         CorruptedShare(share_id: u8) {
