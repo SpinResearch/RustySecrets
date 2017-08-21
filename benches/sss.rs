1e59fcda8f644ebde9f010a9bc169c6bf13da7b5
Romain Ruetschi <romain.ruetschi@gmail.com>
2017-08-21 22:17:18 +0200

    Add benchmarks for sss, wrapped_secrets, thss and t2.

---
+++ benches/sss.rs
@@ -0,0 +1,61 @@
+
+#![cfg(test)]
+#![feature(test)]
+
+extern crate test;
+extern crate rusty_secrets;
+
+mod shared;
+
+mod sss {
+
+    use test::{Bencher, black_box};
+    use rusty_secrets::sss;
+    use shared;
+
+    macro_rules! bench_generate {
+        ($name:ident, $k:expr, $n:expr, $secret:ident, $signed:expr) => (
+            #[bench]
+            fn $name(b: &mut Bencher) {
+                let secret = shared::$secret();
+
+                b.iter(move || {
+                    let shares = sss::generate_shares($k, $n, secret, $signed).unwrap();
+                    black_box(shares);
+                });
+            }
+        )
+    }
+
+    macro_rules! bench_recover {
+        ($name:ident, $k:expr, $n:expr, $secret:ident, $signed:expr) => (
+            #[bench]
+            fn $name(b: &mut Bencher) {
+                let secret = shared::$secret();
+                let all_shares = sss::generate_shares($k, $n, &secret, $signed).unwrap();
+                let shares = &all_shares.into_iter().take($k).collect::<Vec<_>>().clone();
+
+                b.iter(|| {
+                    let result = sss::recover_secret(shares.to_vec(), $signed).unwrap();
+                    black_box(result);
+                });
+            }
+        )
+    }
+
+    bench_generate!(generate_1kb_3_5, 3, 5, secret_1kb, false);
+    bench_recover!(recover_1kb_3_5, 3, 5, secret_1kb, false);
+
+    bench_generate!(generate_1kb_3_5_signed, 3, 5, secret_1kb, true);
+    bench_recover!(recover_1kb_3_5_signed, 3, 5, secret_1kb, true);
+
+    bench_generate!(generate_1kb_10_25, 10, 25, secret_1kb, false);
+    bench_recover!(recover_1kb_10_25, 10, 25, secret_1kb, false);
+
+    bench_generate!(generate_1kb_10_25_signed, 10, 25, secret_1kb, true);
+    bench_recover!(recover_1kb_10_25_signed, 10, 25, secret_1kb, true);
+
+    // bench_generate!(generate_1mb_3_5_unsigned, 3, 5, secret_1mb, false);
+    // bench_recover!(recover_1mb_3_5_unsigned, 3, 5, secret_1mb, false);
+
+}
---
+++ benches/thss.rs
@@ -0,0 +1,57 @@
+
+#![cfg(test)]
+#![feature(test)]
+
+extern crate test;
+extern crate rusty_secrets;
+
+mod shared;
+
+mod thss {
+
+    use rusty_secrets::dss::thss;
+    use test::{Bencher, black_box};
+    use shared;
+
+    macro_rules! bench_generate {
+        ($name:ident, $k:expr, $n:expr, $secret:ident) => (
+            #[bench]
+            fn $name(b: &mut Bencher) {
+                let secret = shared::$secret();
+
+                b.iter(move || {
+                    let scheme = thss::SharingScheme::default();
+                    let shares = scheme.split_secret($k, $n, &secret).unwrap();
+                    black_box(shares);
+                });
+            }
+        )
+    }
+
+    macro_rules! bench_recover {
+        ($name:ident, $k:expr, $n:expr, $secret:ident) => (
+            #[bench]
+            fn $name(b: &mut Bencher) {
+                let secret = shared::$secret();
+                let scheme = thss::SharingScheme::default();
+                let all_shares = scheme.split_secret($k, $n, &secret).unwrap();
+                let shares = &all_shares.into_iter().take($k).collect::<Vec<_>>().clone();
+
+                b.iter(|| {
+                    let result = scheme.recover_secret(&shares.to_vec()).unwrap();
+                    black_box(result);
+                });
+            }
+        )
+    }
+
+    bench_generate!(generate_1kb_3_5, 3, 5, secret_1kb);
+    bench_recover!(recover_1kb_3_5, 3, 5, secret_1kb);
+
+    bench_generate!(generate_1kb_10_25, 10, 25, secret_1kb);
+    bench_recover!(recover_1kb_10_25, 10, 25, secret_1kb);
+
+    // bench_generate!(generate_1mb_3_5, 3, 5, secret_1mb);
+    // bench_recover!(recover_1mb_3_5, 3, 5, secret_1mb);
+
+}
