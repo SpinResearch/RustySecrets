f4f962f97c459ab1cb0d68bb734df00af0b4d5d4
Romain Ruetschi <romain.ruetschi@gmail.com>
2017-09-09 18:20:42 +0200

    Split ThSS struct out of thss mod file.

--- src/dss/thss/mod.rs
+++ src/dss/thss/mod.rs
@@ -1,18 +1,15 @@
 
 //! Simple threshold secret sharing scheme
 
-use ring::rand::{SecureRandom, SystemRandom};
-
 use errors::*;
-use dss::random::{get_random_bytes, random_len};
-use lagrange::lagrange_interpolate;
-use share::validation::validate_shares;
+
+mod math;
 
 mod share;
 pub use self::share::*;
 
-mod math;
-use self::math::encode_secret;
+mod scheme;
+pub(crate) use self::scheme::ThSS;
 
 /// TODO: Doc
 pub fn split_secret(
@@ -27,90 +24,6 @@ pub fn split_secret(
 /// TODO: Doc
 pub fn recover_secret(shares: &[Share]) -> Result<(Vec<u8>, Option<MetaData>)> {
     ThSS::default().recover_secret(shares)
-}
-
-/// A simple threshold sharing scheme
-#[allow(missing_debug_implementations)]
-pub(crate) struct ThSS {
-    /// The randomness source
-    pub random: Box<SecureRandom>,
-}
-
-impl Default for ThSS {
-    fn default() -> Self {
-        Self::new(Box::new(SystemRandom::new()))
-    }
-}
-
-impl ThSS {
-    /// Constructs a new sharing scheme
-    pub fn new(random: Box<SecureRandom>) -> Self {
-        Self { random }
-    }
-
-    /// Split a secret following a given sharing `scheme`,
-    /// with `threshold` being the number of shares necessary to recover the secret,
-    /// and `total_shares_count` the total number of shares to be dealt.
-    pub fn split_secret(
-        &self,
-        threshold: u8,
-        total_shares_count: u8,
-        secret: &[u8],
-        metadata: &Option<MetaData>,
-    ) -> Result<Vec<Share>> {
-        if threshold < 1 || total_shares_count < 1 {
-            bail!(ErrorKind::InvalidSplitParametersZero(
-                threshold,
-                total_shares_count,
-            ));
-        }
-
-        if threshold > total_shares_count {
-            bail!(ErrorKind::InvalidThreshold(threshold, total_shares_count));
-        }
-
-        let rands_len = random_len(threshold as usize, secret.len());
-        let rands = get_random_bytes(self.random.as_ref(), rands_len)?;
-
-        let shares = (0..total_shares_count)
-            .map(|id| {
-                let data = encode_secret(secret, threshold, id, &rands);
-
-                Share {
-                    id,
-                    threshold,
-                    total_shares_count,
-                    data,
-                    metadata: metadata.clone(),
-                }
-            })
-            .collect();
-
-        Ok(shares)
-    }
-
-    /// Recover the secret from the given set of shares
-    pub fn recover_secret(&self, shares: &[Share]) -> Result<(Vec<u8>, Option<MetaData>)> {
-        let (_, shares) = validate_shares(shares.to_vec())?;
-
-        // FIXME: Check that the data length is the same for all shares.
-        let m = shares[0].data.len();
-
-        let secret = (0..m)
-            .map(|i| {
-                let points = shares
-                    .iter()
-                    .map(|share| (share.id, share.data[i]))
-                    .collect::<Vec<_>>();
-
-                lagrange_interpolate(&points)
-            })
-            .collect();
-
-        let metadata = shares[0].metadata.clone();
-
-        Ok((secret, metadata))
-    }
 }
 
 #[cfg(test)]
---
+++ src/dss/thss/scheme.rs
@@ -0,0 +1,96 @@
+
+//! Simple threshold secret sharing scheme
+
+use ring::rand::{SecureRandom, SystemRandom};
+
+use errors::*;
+use dss::random::{get_random_bytes, random_len};
+use lagrange::lagrange_interpolate;
+use share::validation::validate_shares;
+
+use super::share::*;
+use super::math::encode_secret;
+
+/// A simple threshold sharing scheme
+#[allow(missing_debug_implementations)]
+pub(crate) struct ThSS {
+    /// The randomness source
+    pub random: Box<SecureRandom>,
+}
+
+impl Default for ThSS {
+    fn default() -> Self {
+        Self::new(Box::new(SystemRandom::new()))
+    }
+}
+
+impl ThSS {
+    /// Constructs a new sharing scheme
+    pub fn new(random: Box<SecureRandom>) -> Self {
+        Self { random }
+    }
+
+    /// Split a secret following a given sharing `scheme`,
+    /// with `threshold` being the number of shares necessary to recover the secret,
+    /// and `total_shares_count` the total number of shares to be dealt.
+    pub fn split_secret(
+        &self,
+        threshold: u8,
+        total_shares_count: u8,
+        secret: &[u8],
+        metadata: &Option<MetaData>,
+    ) -> Result<Vec<Share>> {
+        if threshold < 1 || total_shares_count < 1 {
+            bail!(ErrorKind::InvalidSplitParametersZero(
+                threshold,
+                total_shares_count,
+            ));
+        }
+
+        if threshold > total_shares_count {
+            bail!(ErrorKind::InvalidThreshold(threshold, total_shares_count));
+        }
+
+        let rands_len = random_len(threshold as usize, secret.len());
+        let rands = get_random_bytes(self.random.as_ref(), rands_len)?;
+
+        let shares = (0..total_shares_count)
+            .map(|id| {
+                let data = encode_secret(secret, threshold, id, &rands);
+
+                Share {
+                    id,
+                    threshold,
+                    total_shares_count,
+                    data,
+                    metadata: metadata.clone(),
+                }
+            })
+            .collect();
+
+        Ok(shares)
+    }
+
+    /// Recover the secret from the given set of shares
+    pub fn recover_secret(&self, shares: &[Share]) -> Result<(Vec<u8>, Option<MetaData>)> {
+        let (_, shares) = validate_shares(shares.to_vec())?;
+
+        // FIXME: Check that the data length is the same for all shares.
+        let m = shares[0].data.len();
+
+        let secret = (0..m)
+            .map(|i| {
+                let points = shares
+                    .iter()
+                    .map(|share| (share.id, share.data[i]))
+                    .collect::<Vec<_>>();
+
+                lagrange_interpolate(&points)
+            })
+            .collect();
+
+        let metadata = shares[0].metadata.clone();
+
+        Ok((secret, metadata))
+    }
+}
