208bbd66da3a10a248192b6e76b83be662363e7b
dtsbourg <dylan.bourgeois@epfl.ch>
2017-09-14 14:26:27 +0200

    thss - generation_errors

---
+++ tests/thss_generation_errors.rs
@@ -0,0 +1,12 @@
+extern crate rusty_secrets;
+
+use rusty_secrets::dss::thss;
+
+#[test]
+#[should_panic(expected = "ThresholdTooBig")]
+fn test_generate_invalid_k() {
+    let secret = "These programs were never about terrorism: they’re about economic spying, \
+                  social control, and diplomatic manipulation. They’re about power.";
+
+    thss::split_secret(10, 7, &secret.as_bytes(), &None).unwrap();
+}
