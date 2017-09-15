39895a8cd7e7c5c54344228f7947f048053dc5aa
Romain Ruetschi <romain.ruetschi@gmail.com>
2017-09-15 21:06:53 +0200

    Fix a ThSS test.

--- tests/thss_generation_errors.rs
+++ tests/thss_generation_errors.rs
@@ -5,8 +5,8 @@ use rusty_secrets::dss::thss;
 #[test]
 #[should_panic(expected = "ThresholdTooBig")]
 fn test_generate_invalid_k() {
-    let secret = "These programs were never about terrorism: they’re about economic spying, \
-                  social control, and diplomatic manipulation. They’re about power.";
+    let secret = b"These programs were never about terrorism: they're about economic spying, \
+                  social control, and diplomatic manipulation. They're about power.";
 
     thss::split_secret(10, 7, secret, &None).unwrap();
 }
